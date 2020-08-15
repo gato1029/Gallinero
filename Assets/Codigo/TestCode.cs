6fd6687e3343948524860c021ff36a52e0c58884
josue <josue.ccama@gmail.com>
2020-08-15 10:37:09 -0500

    subiendo codigo

---
+++ Assets/Codigo/TestCode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCode : MonoBehaviour
+{
+    void prueba()
+    { 
+        //esto es una prueba master branch
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
